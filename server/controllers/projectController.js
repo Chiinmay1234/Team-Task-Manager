065eddcfb27a6796a96bf782328c09fb3c5f56a7
Chinmayi Sharma <chinmayisharma@Chinmayis-MacBook-Pro.local>
2026-05-07 23:10:45 +0530

    Improved project management backend

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,38 +1,61 @@
 const Project = require("../models/Project");
 
+// CREATE PROJECT
 const createProject = async (req, res) => {
+
   try {
+
     const { title, description, teamMembers } = req.body;
+
+    if (!title) {
+      return res.status(400).json({
+        message: "Project title is required",
+      });
+    }
 
     const project = await Project.create({
       title,
       description,
-      teamMembers,
+      teamMembers: teamMembers || [],
       createdBy: req.user.id,
     });
 
-    res.status(201).json(project);
+    const populatedProject = await Project.findById(project._id)
+      .populate("createdBy", "name email")
+      .populate("teamMembers", "name email");
+
+    res.status(201).json(populatedProject);
 
   } catch (error) {
+
     res.status(500).json({
       message: error.message,
     });
+
   }
+
 };
 
+// GET ALL PROJECTS
 const getProjects = async (req, res) => {
+
   try {
+
     const projects = await Project.find()
       .populate("createdBy", "name email")
-      .populate("teamMembers", "name email");
+      .populate("teamMembers", "name email")
+      .sort({ createdAt: -1 });
 
     res.status(200).json(projects);
 
   } catch (error) {
+
     res.status(500).json({
       message: error.message,
     });
+
   }
+
 };
 
 module.exports = {
